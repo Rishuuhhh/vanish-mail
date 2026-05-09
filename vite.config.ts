1bbdf7b67fe6cea196a9bc27b5e08b9141b37f40
Rishuuhhh <sharmarakshit3000@gmail.com>
2026-05-09 21:54:22 +0530

    fix: vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,6 @@
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig(({ command }) => ({
-  plugins: [
-    ...tanstackStart({
-      customViteReactPlugin: true,
-      tsr: {
-        autoCodeSplitting: true,
-      },
-      server: {
-        entry: "server",
-      },
-    }),
-    react(),
-    tailwindcss(),
-    tsConfigPaths(),
-    ...(command === "build" ? [cloudflare()] : []),
-  ],
-}));
+export default defineConfig({
+  plugins: [react()],
+});
